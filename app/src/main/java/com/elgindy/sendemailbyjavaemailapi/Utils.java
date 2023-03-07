93f6d94d40cca3ce8c5f3edd5d17cb4ea897f52f
Miguel de Jesús Martínez Felipe <mmartinezf2002@alumno.ipn.mx>
2023-03-07 17:08:48 -0600

    Submit

---
+++ app/src/main/java/com/elgindy/sendemailbyjavaemailapi/Utils.java
@@ -0,0 +1,8 @@
+package com.elgindy.sendemailbyjavaemailapi;
+
+public class Utils {
+    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
+
+    public static final  String EMAIL = "miguelillnino@gmail.com";
+    public static final String PASSWORD = "*****";
+}
